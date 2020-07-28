a256c15da4650a5955cd3a9b808589006f80d1dc
Walter Abregu <w.abregu@comunal.co>
2020-07-28 00:47:02 -0500

    agregado de un componente

---
+++ src/app/ejercicios/ejercicios.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ejercicios',
+  templateUrl: './ejercicios.component.html',
+  styleUrls: ['./ejercicios.component.css']
+})
+export class EjerciciosComponent implements OnInit {
+
+  ejercicios = 'Angular es grande !!';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
